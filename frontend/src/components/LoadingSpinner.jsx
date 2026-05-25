a29499587212e3fbf3dec799d6ec5391f7f43da0
unknown <firmanajah366@gmail.com>
2026-05-25 22:07:59 +0700

    Update loading spinner

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import logoImg from "../assets/logo.png";
 import "./LoadingSpinner.css";
 
-export default function LoadingSpinner({ message = "Memuat...", fullPage = false }) {
+export default function LoadingSpinner({ fullPage = false }) {
   return (
     <div className={`loading-spinner-container ${fullPage ? "full-page" : ""}`}>
       <div className="logo-loading-box">
         <img src={logoImg} alt="LeMaS Logo" className="logo-loading-img" />
       </div>
-      {message && <p className="loading-spinner-message">{message}</p>}
     </div>
   );
 }
